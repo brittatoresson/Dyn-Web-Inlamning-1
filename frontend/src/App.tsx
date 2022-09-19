92aa4e076259e57adde048579c646c927680175b
brittatoresson <britta_toresson@hotmail.com>
2022-09-19 11:43:46 +0200

    login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
-
+import SignUp from "./signUp";
 function App() {
-    return <div className="App">Bröllopsfotografen</div>;
+  return (
+    <div className="App">
+      Bröllopsfotografen
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
