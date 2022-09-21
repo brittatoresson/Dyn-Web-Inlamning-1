f8a10435dd45e553ebe45dc091a020ca6205077e
marcus-df <marcusfalck@fastmail.fm>
2022-09-21 16:18:13 +0200

    working version

--- frontend/src/pages/GalleryPage.tsx
+++ frontend/src/pages/GalleryPage.tsx
@@ -2,63 +2,63 @@ import { useEffect, useState } from "react";
 import xMark from "../assets/xmark-solid.svg";
 
 function GalleryPage() {
-  const [galleryImages, setGalleryImages] = useState<Array<object>>([
-    { savedPhoto: "", userID: "", _id: "" },
-  ]);
+    const [galleryImages, setGalleryImages] = useState<Array<object>>([
+        { savedPhoto: "", userID: "", _id: "" },
+    ]);
 
-  async function getGalleryImages() {
-    let userID: string | undefined | null = localStorage.getItem("user-id");
-    userID = userID?.substring(1, userID.length - 1);
+    async function getGalleryImages() {
+        let userID: string | undefined | null = localStorage.getItem("user-id");
+        userID = userID?.substring(1, userID.length - 1);
 
-    const response = await fetch("http://localhost:5555/api/photodb", {
-      method: "GET",
-      headers: {
-        authorization: `user-id: ${userID}`,
-      },
-    });
-    const data = await response.json();
-    setGalleryImages(await data);
-  }
+        const response = await fetch("http://localhost:5555/api/photodb", {
+            method: "GET",
+            headers: {
+                authorization: `user-id: ${userID}`,
+            },
+        });
+        const data = await response.json();
+        setGalleryImages(await data);
+    }
 
-  async function removeImage(image: { userID: ""; _id: "" }) {
-    console.log(image);
+    async function removeImage(image: { userID: ""; _id: "" }) {
+        console.log(image);
 
-    const sendData = {
-      userID: image.userID,
-      imageID: image._id,
-    };
+        const sendData = {
+            userID: image.userID,
+            imageID: image._id,
+        };
 
-    const response = await fetch("http://localhost:5555/api/photodb", {
-      method: "DELETE",
-      body: JSON.stringify(sendData),
-      headers: { "Content-Type": "application/json" },
-    });
-    const data = await response.json();
-    console.log(data);
-    getGalleryImages();
-  }
+        const response = await fetch("http://localhost:5555/api/photodb", {
+            method: "DELETE",
+            body: JSON.stringify(sendData),
+            headers: { "Content-Type": "application/json" },
+        });
+        const data = await response.json();
+        console.log(data);
+        getGalleryImages();
+    }
 
-  return (
-    <section>
-      <h1>Gallery</h1>
-      <article className="gallery-grid">
-        {galleryImages.map((imgData: any, i: number) => (
-          <div className="gallery-img-box" key={i}>
-            <img
-              className="gallery-img"
-              src={imgData?.savedPhoto}
-              alt="webcam"
-            />
-            <img
-              className="gallery-img-btn"
-              src={xMark}
-              onClick={() => removeImage(imgData)}
-            />
-          </div>
-        ))}
-      </article>
-    </section>
-  );
+    useEffect(() => {
+        getGalleryImages();
+    }, []);
+
+    return (
+        <section>
+            <h1>Gallery</h1>
+            <article className="gallery-grid">
+                {galleryImages.map((imgData: any, i: number) => (
+                    <div className="gallery-img-box" key={i}>
+                        <img className="gallery-img" src={imgData.savedPhoto} alt="webcam" />
+                        <img
+                            className="gallery-img-btn"
+                            src={xMark}
+                            onClick={() => removeImage(imgData)}
+                        />
+                    </div>
+                ))}
+            </article>
+        </section>
+    );
 }
 
 export default GalleryPage;
