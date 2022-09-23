ae1aab2ccc0784373ee50ff628414b9d7e665a7b
brittatoresson <britta_toresson@hotmail.com>
2022-09-23 11:44:11 +0200

    filter, frame

--- frontend/src/components/Filter.tsx
+++ frontend/src/components/Filter.tsx
@@ -5,13 +5,16 @@ export default function filterFunction(
   if (filter == "grayscale(100%)") {
     context.filter = "grayscale(100%)";
   }
-  if (filter == "hue-rotate(90deg)") {
-    context.filter = "hue-rotate(90deg)";
+  if (filter == "blur") {
+    context.filter = "blur(5px)";
   }
-  if (filter == "old") {
-    context.filter = "grayscale(1) sepia(0.5) brightness(1.3) invert(0.8);";
+  if (filter == "sepia") {
+    context.filter = "sepia(1)";
   }
   if (filter == "low_fi") {
     context.filter = "saturate(1.1) contrast(1.5)";
+  }
+  if (filter == "contrast") {
+    context.filter = "contrast(2.5)";
   }
 }
